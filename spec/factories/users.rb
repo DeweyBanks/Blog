28f352eebab6061f6caed573b537520399f9ee7b
deweybanks <deweybanks@gmail.com>
2019-02-16 12:34:47 -0500

    add login feature spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@test.com" }
-    password "blah1234"
-    password_confirmation "blah1234"
+    sequence(:password) { |n| "blah1234"}
+    sequence(:password_confirmation) { |n| "blah1234"}
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include FactoryBot::Syntax::Methods
   Capybara.javascript_driver = :poltergeist
